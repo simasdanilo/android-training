e4baac8be386c1ac09ccd2558c13e34e8ee46c04
Danilo Simas <simas.danilo@gmail.com>
2019-02-21 20:58:24 -0300

    feat: implements lesson 1.2

--- app/src/main/java/com/simasdanilo/training/MainActivity.java
+++ app/src/main/java/com/simasdanilo/training/MainActivity.java
@@ -2,12 +2,34 @@ package com.simasdanilo.training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    private TextView countText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.countText = findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        count++;
+        updateCountText(count);
+    }
+
+    private void updateCountText(int count){
+        String countString = Integer.toString(count);
+        if (countText != null)
+            countText.setText(countString);
     }
 }
