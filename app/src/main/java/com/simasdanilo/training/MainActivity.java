c1cb5717bf481563d36fa22b06c1bd626c737f62
Danilo Simas <simas.danilo@gmail.com>
2019-03-02 14:14:58 -0300

    feature: implements lesson 2.2 Activity lifecycle and state

--- app/src/main/java/com/simasdanilo/training/MainActivity.java
+++ app/src/main/java/com/simasdanilo/training/MainActivity.java
@@ -3,6 +3,7 @@ package com.simasdanilo.training;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,10 +12,92 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE =
             "com.simasdanilo.training.extra.MESSAGE";
     private static final int TEXT_REQUEST = 1;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-    private TextView replyHeaderText;
-    private TextView replyText;
+    private static final String STATE_REPLY_VISIBLE = "reply_visible";
+    private static final String STATE_REPLY_TEXT = "reply_text";
+
+    private TextView replyHeaderTextView;
+    private TextView replyTextView;
     private EditText messageEditText;
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (replyHeaderTextView.getVisibility() == View.VISIBLE) {
+            final String replyText = replyTextView.getText().toString();
+
+            outState.putString(STATE_REPLY_TEXT, replyText);
+            outState.putBoolean(STATE_REPLY_VISIBLE, true);
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.bindViews();
+        Log.d(LOG_TAG, "-----------------");
+        Log.d(LOG_TAG, "onCreate");
+        // Restore the state.
+        if (savedInstanceState != null) {
+            this.restoreInstanceState(savedInstanceState);
+        }
+    }
+
+    private void restoreInstanceState(Bundle savedInstanceState) {
+        boolean replyIsVisible = savedInstanceState.getBoolean(STATE_REPLY_VISIBLE);
+
+        if (replyIsVisible){
+            String reply = savedInstanceState.getString(STATE_REPLY_TEXT);
+            this.restoreReplyViewsState(reply);
+        }
+
+    }
+
+    private void restoreReplyViewsState(String reply) {
+        this.replyHeaderTextView.setVisibility(View.VISIBLE);
+        this.replyTextView.setVisibility(View.VISIBLE);
+        this.replyTextView.setText(reply);
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
 
 
     @Override
@@ -25,6 +108,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void bindViews() {
+        this.messageEditText = findViewById(R.id.editText_message);
+        this.replyHeaderTextView = findViewById(R.id.text_header_reply);
+        this.replyTextView = findViewById(R.id.text_reply);
+    }
+
     private void handleTextRequest(int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
             this.showReplyReceived(data);
@@ -33,23 +122,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void showReplyReceived(Intent data) {
         String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-        this.replyHeaderText.setVisibility(View.VISIBLE);
-        this.replyText.setText(reply);
-        this.replyText.setVisibility(View.VISIBLE);
-    }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        this.bindViews();
-    }
-
-    private void bindViews() {
-        this.messageEditText = findViewById(R.id.editText_message);
-        this.replyHeaderText = findViewById(R.id.text_header_reply);
-        this.replyText = findViewById(R.id.text_reply);
+        this.replyHeaderTextView.setVisibility(View.VISIBLE);
+        this.replyTextView.setText(reply);
+        this.replyTextView.setVisibility(View.VISIBLE);
     }
 
     private String getMessageToSend(){
--- app/src/main/java/com/simasdanilo/training/SecondActivity.java
+++ app/src/main/java/com/simasdanilo/training/SecondActivity.java
@@ -3,6 +3,7 @@ package com.simasdanilo.training;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 public class SecondActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY =
             "com.simasdanilo.training.extra.REPLY";
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
 
     private EditText replyEditText;
 
@@ -19,6 +21,44 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         this.bindViews();
         this.showMessageReceived();
+        Log.d(LOG_TAG, "-------------------");
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 
     private void bindViews() {
@@ -54,6 +94,7 @@ public class SecondActivity extends AppCompatActivity {
 
         this.setResult(RESULT_OK,replyIntent);
         this.finish();
+        Log.d(LOG_TAG, "End SecondActivity");
     }
 }
 
