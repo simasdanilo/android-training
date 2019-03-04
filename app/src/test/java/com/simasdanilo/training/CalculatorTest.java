a53a3342a6d79a96274fef685d31d42e5d0b780e
Danilo Simas <simas.danilo@gmail.com>
2019-03-04 09:48:29 -0300

    feat: implements lesson 3.1: The debugger

---
+++ app/src/test/java/com/simasdanilo/training/CalculatorTest.java
@@ -0,0 +1,41 @@
+package com.simasdanilo.training;
+
+// import android.support.test.filters.SmallTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * JUnit4 unit tests for the calculator logic. These are local unit tests; no device needed
+ */
+@RunWith(JUnit4.class)
+
+// @SmallTest
+public class CalculatorTest {
+
+    private Calculator mCalculator;
+
+    /**
+     * Set up the environment for testing
+     */
+    @Before
+    public void setUp() {
+        mCalculator = new Calculator();
+    }
+
+    /**
+     * Test for simple addition
+     */
+    @Test
+    public void addTwoNumbers() {
+        double resultAdd = mCalculator.add(1d, 1d);
+        assertThat(resultAdd, is(equalTo(2d)));
+    }
+
+}
