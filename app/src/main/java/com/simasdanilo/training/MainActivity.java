43f4c7abf270ab7cf4e1100bf6db442a03dddbab
Danilo Simas <simas.danilo@gmail.com>
2019-03-05 15:44:35 -0300

    feat: implements lesson 4.4: User navigation

--- app/src/main/java/com/simasdanilo/training/MainActivity.java
+++ app/src/main/java/com/simasdanilo/training/MainActivity.java
@@ -1,7 +1,10 @@
 package com.simasdanilo.training;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setupViews();
+
     }
+
+    private void setupViews() {
+        // Create an instance of the tab layout from the view.
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        this.setupToolbar();
+        this.setupTabBar(tabLayout);
+        this.setupPagerAdapter(tabLayout);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setupTabBar(TabLayout tabLayout) {
+        // Set the text for each tab.
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label3));
+
+        // Set the tabs to fill the entire layout.
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+    }
+
+    private void setupPagerAdapter(TabLayout tabLayout) {
+        // Use PagerAdapter to manage page views in fragments.
+        // Each page is represented by its own fragment.
+        final ViewPager viewPager = findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        // Setting a listener for clicks.
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(
+                new TabLayout.OnTabSelectedListener() {
+                                                       @Override
+                                                       public void onTabSelected(TabLayout.Tab tab) {
+                                                           viewPager.setCurrentItem(tab.getPosition());
+                                                       }
+
+                                                       @Override
+                                                       public void onTabUnselected(TabLayout.Tab tab) {
+                                                       }
+
+                                                       @Override
+                                                       public void onTabReselected(TabLayout.Tab tab) {
+                                                       }
+                                                   });
+    }
+
+
 }
