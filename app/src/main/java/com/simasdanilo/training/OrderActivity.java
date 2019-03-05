50612d107afbb50a93c8f62f3fe2ed686f58451f
Danilo Simas <simas.danilo@gmail.com>
2019-03-05 10:45:58 -0300

    feat: implements lesson 4.2: Input controls

--- app/src/main/java/com/simasdanilo/training/OrderActivity.java
+++ app/src/main/java/com/simasdanilo/training/OrderActivity.java
@@ -3,18 +3,48 @@ package com.simasdanilo.training;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-        this.readIntent();
+        this.loadIntent();
+        this.setupSpinner();
     }
 
-    private void readIntent() {
+    private void setupSpinner() {
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner == null) {
+            return;
+        }
+
+        // Create ArrayAdapter using the string array and default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner.
+        spinner.setAdapter(adapter);
+
+        // Create the spinner.
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+
+    private void loadIntent() {
         Intent intent = getIntent();
         String orderMessage = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
@@ -27,4 +57,45 @@ public class OrderActivity extends AppCompatActivity {
         textView.setText(message);
     }
 
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        RadioButton radioButton = (RadioButton) view;
+        boolean checked = radioButton.isChecked();
+        int radioId = radioButton.getId();
+
+        // Check which radio button was clicked.
+        switch (radioId) {
+            case R.id.sameday:
+                if (checked)
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // Do nothing.
+                break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String spinnerLabel = parent.getItemAtPosition(position).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
